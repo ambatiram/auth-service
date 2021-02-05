256c1666f5da6aa8cf3e04350609d42e3bc81892
AmbatiRa <AmbatiRa@IN-Ambatira.ap.uis.unisys.com>
2021-02-05 12:14:22 +0530

    Added Cors Configuration

--- src/main/java/com/ram/authservice/config/WebMvcConfig.java
+++ src/main/java/com/ram/authservice/config/WebMvcConfig.java
@@ -1,9 +1,11 @@
 package com.ram.authservice.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer{
 	private final long MAX_AGE_SECS = 3600;
 
